8ae40e0ad037fb683b169d9add968641aa333f68
finlayrossdavie <39996934+finlayrossdavie@users.noreply.github.com>
2025-06-09 15:14:35 +0100

    improved graphs

--- systems/newsystems/goldspread/main.py
+++ systems/newsystems/goldspread/main.py
@@ -14,12 +14,16 @@ thresholds = [(0.15, 0.25), (0.2, 0.3), (0.25, 0.35),
 plt.figure(figsize=(12, 7))
 
 for i, (entry, exit) in enumerate(thresholds):
+
     config = Config(entry_threshold=entry, exit_threshold=exit, asset1='GLD', asset2='GDX', initial_capital=100000)
     portfolio = Portfolio(prices, config)
     portfolio.backtest('2014-05-23', '2019-05-23') #just testing a recent pre-covid 5 year, leave empty to backtest the full dataset
-    pnl = [x.get_pnl() for x in portfolio.positions if isinstance(x, Position)]
+    pnl = [(x.get_pnl()) for x in portfolio.positions if isinstance(x, Position)]
+    close_dates = [x.exit_date for x in portfolio.positions if isinstance(x, Position)]
     portfolios.append(portfolio)   
-    plt.plot(prices.index[800:800+len(pnl)], pnl, label=f'Entry={entry}, Exit={exit}')
+
+
+    plt.plot(close_dates, pnl, label=f'Entry={entry}, Exit={exit}')
 
 
 plt.legend()
